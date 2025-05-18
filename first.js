c2ed3f7cbe37cfb276068f1abc516003882eacae
SehwarMaryam <maryam.dev93@gmail.com>
2025-05-18 22:14:25 +0500

    initial commit of outer and inner function structure

--- first.js
+++ first.js
@@ -8,22 +8,3 @@ function outer() {
 }
 outer();
 
-function increment() {
-    let count = 0;
-    count++;
-    console.log(count);
-}
-increment();
-
-
-
-function createCounter(){
-  function increment() {
-       let count = 0;
-    count++;
-    console.log(count);
-}
-return {increment};
-}
-const counter = createCounter();
-counter.increment(); // 1
