3e93bdc0e548fd1510140eb09a355ae689282e7b
SehwarMaryam <maryam.dev93@gmail.com>
2025-05-19 18:11:31 +0500

    Update background colors and fix alignment in first.js

--- first.js
+++ first.js
@@ -1,6 +1,6 @@
 // 1.document.getElementById("") 
 const myHeading= document.getElementById("my-heading");
-myHeading.style.backgroundColor = "purple";
+myHeading.style.backgroundColor = "lightpurple";
 myHeading.style.textAlighn= "centre";
 console.log(myHeading);
 
@@ -16,9 +16,36 @@ for(let fruit of fruits){
 }
 
 Array.from(fruits).forEach(fruit => {
-    fruit.style.backgroundColor = "red";
+    fruit.style.backgroundColor = "lightred";
 });
 
 // 3.document.getElementsByTagName("")
 const h4Elements = document.getElementsByTagName("h4");
+const liElements = document.getElementsByTagName("li");
+
 console.log(h4Elements);
+
+h4Elements[0].style.backgroundColor = "red";
+ 
+for(let h4Element of h4Elements){
+    h4Element.style.backgroundColor = "pink";
+}
+
+for(let liElement of liElements){
+    liElement.style.backgroundColor = "pink";
+}
+Array.from(liElements).forEach(liElement => {
+    liElement.style.backgroundColor = "lightblue";
+})
+
+// 4.document.querySelector("")
+
+const element = document.querySelector(".fruits");
+element.style.backgroundColor = "lightgreen";
+
+// 5.document.querySelectorAll("")
+
+const foods = document.querySelectorAll("li");
+foods[0].style.backgroundColor = "lightblue";
+foods[1].style.backgroundColor = "lightgreen";
+foods[2].style.backgroundColor = "lightyellow";
