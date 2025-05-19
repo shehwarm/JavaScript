bc7322f93c1c4a3f968c1c86b2d4291c9a307aee
SehwarMaryam <maryam.dev93@gmail.com>
2025-05-19 19:45:49 +0500

    Refactor HTML structure and update JavaScript to set background color for first list items

--- first.js
+++ first.js
@@ -1,55 +1,11 @@
-// 1.document.getElementById("") 
-const myHeading= document.getElementById("my-heading");
-myHeading.style.backgroundColor = "lightpurple";
-myHeading.style.textAlighn= "centre";
-console.log(myHeading);
+//.firstElementChild
 
-// 2.document.getElementsByClassName("")
-const fruits = document.getElementsByClassName("fruits");
-fruits[0].style.backgroundColor = "red";
-fruits[1].style.backgroundColor = "orange";
-fruits[2].style.backgroundColor = "yellow";
-console.log(fruits);
+const element = document.getElementById("fruits");
+const firstChild = element.firstElementChild;
+firstChild.style.backgroundColor = "red";
 
-for(let fruit of fruits){
-    fruit.style.backgroundColor = "yellow";
-}
-
-Array.from(fruits).forEach(fruit => {
-    fruit.style.backgroundColor = "lightred";
-});
-
-// 3.document.getElementsByTagName("")
-const h4Elements = document.getElementsByTagName("h4");
-const liElements = document.getElementsByTagName("li");
-
-console.log(h4Elements);
-
-h4Elements[0].style.backgroundColor = "red";
- 
-for(let h4Element of h4Elements){
-    h4Element.style.backgroundColor = "pink";
-}
-
-for(let liElement of liElements){
-    liElement.style.backgroundColor = "pink";
-}
-Array.from(liElements).forEach(liElement => {
-    liElement.style.backgroundColor = "lightblue";
-})
-
-// 4.document.querySelector("")
-
-const element = document.querySelector(".fruits");
-element.style.backgroundColor = "lightgreen";
-
-// 5.document.querySelectorAll("")
-
-const foods = document.querySelectorAll("li");
-foods[0].style.backgroundColor = "lightblue";
-foods[1].style.backgroundColor = "lightgreen";
-foods[2].style.backgroundColor = "lightyellow";
-
-foods.forEach(food => {
-    food.style.backgroundColor = "lightpink";
+const ulElemnts = document.querySelectorAll("ul");
+ulElemnts.forEach((ul) => {
+  const firstChild = ul.firstElementChild;
+  firstChild.style.backgroundColor = "red";
 });
