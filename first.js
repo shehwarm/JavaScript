c963a8c529bd55e9ab123df6126dfe69680d34c4
SehwarMaryam <maryam.dev93@gmail.com>
2025-05-19 19:53:02 +0500

    Fix background color assignment for last child of fruits element and update all list items

--- first.js
+++ first.js
@@ -3,4 +3,10 @@
 
 const element = document.getElementById("fruits");
 const lastChild = element.lastElementChild;
-lastChild.style.backgroundClor = yellow;
+lastChild.style.backgroundColor = "yellow";
+
+const ulElements = document.querySelectorAll("ul");
+ulElements.forEach((ul) => {
+    const lastChild = ul.lastElementChild;
+    lastChild.style.backgroundColor = "yellow";
+});
