a321a699cb603387b96e34352242481c92852535
SehwarMaryam <maryam.dev93@gmail.com>
2025-05-19 17:49:48 +0500

    Refactor HTML structure and update JavaScript for dynamic styling of elements

--- first.js
+++ first.js
@@ -1,7 +1,24 @@
-document.title = "website";
-document.body.style.backgroundColor = "black";
-console.dir(document); 
+// 1.document.getElementById("") 
+const myHeading= document.getElementById("my-heading");
+myHeading.style.backgroundColor = "purple";
+myHeading.style.textAlighn= "centre";
+console.log(myHeading);
 
-const username = " Shehwar ";
-const welcomeMsg = document.getElementById("welcome-msg");
-welcomeMsg.textContent += username === "" ? `Guest` : username;
+// 2.document.getElementsByClassName("")
+const fruits = document.getElementsByClassName("fruits");
+fruits[0].style.backgroundColor = "red";
+fruits[1].style.backgroundColor = "orange";
+fruits[2].style.backgroundColor = "yellow";
+console.log(fruits);
+
+for(let fruit of fruits){
+    fruit.style.backgroundColor = "yellow";
+}
+
+Array.from(fruits).forEach(fruit => {
+    fruit.style.backgroundColor = "red";
+});
+
+// 3.document.getElementsByTagName("")
+const h4Elements = document.getElementsByTagName("h4");
+console.log(h4Elements);
