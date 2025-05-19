33edac2a5ef4fa3c8794669c7fc7f741211c381d
SehwarMaryam <maryam.dev93@gmail.com>
2025-05-19 19:49:05 +0500

    Fix background color assignment for last child of fruits element

--- first.js
+++ first.js
@@ -1,11 +1,6 @@
-//.firstElementChild
+
+//.lastElementChild
 
 const element = document.getElementById("fruits");
-const firstChild = element.firstElementChild;
-firstChild.style.backgroundColor = "red";
-
-const ulElemnts = document.querySelectorAll("ul");
-ulElemnts.forEach((ul) => {
-  const firstChild = ul.firstElementChild;
-  firstChild.style.backgroundColor = "red";
-});
+const lastChild = element.lastElementChild;
+lastChild.style.backgroundClor = yellow;
