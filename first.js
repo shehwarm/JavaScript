8c2be97dbcfec1f2adb16253ff9134b7a7b3c241
SehwarMaryam <maryam.dev93@gmail.com>
2025-05-20 22:09:36 +0500

    Remove unused element creation and properties; simplify color change function for myBox click event

--- first.js
+++ first.js
@@ -1,19 +1,11 @@
 
 
-// step 1 : create the element
+const myBox = document.getElementById("myBox");
 
-const newH1 = document.createElement("h1");
+function changeColor(event){
+        event.target.style.backgroundColor = "red";
+        
+    }
 
-//step 2 : add the properties
-
-newH1.textContent = "Hello World";
-newH1.id = "myH1";
-newH1.style.color = "red";
-newH1.style.fontSize = "50px";
-newH1.style.textAlign = "center";
-
-//step 3 : add the element to the DOM
-document.body.append(newH1);
-
-//step 4 : Remove HTML element
-
+myBox.addEventListener("click", changeColor);
+ 
