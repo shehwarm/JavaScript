ee3ee7c01b397b5aa000209d3f49150949e51f9a
SehwarMaryam <maryam.dev93@gmail.com>
2025-05-20 15:19:49 +0500

    Update JavaScript to change background color of vegetable children to yellow

--- first.js
+++ first.js
@@ -1,12 +1,2 @@
 
-//.previousElementSibling
-const element = document.getElementById("apple");
-const previousSibling = element.previousElementSibling;
-previousSibling.style.color = "red";    
 
-
-const ulElements = document.querySelectorAll("ul");
-ulElements.forEach((ul) =>{
-    const previousSibling = ul.previousElementSibling;
-    previousSibling.style.backgroundColor = "red";  
-});
