9a1886569791de6dc80b42cb1a27997af168130c
SehwarMaryam <shehwarm93@gmail.com>
2025-05-22 20:59:50 +0500

    Refactor button implementation: replace multiple buttons with a single button, update styles, and enhance click functionality

--- first.js
+++ first.js
@@ -1,2 +1,36 @@
-let buttons = document.querySelectorAll(".myButtons");
+const myButton = document.getElementById("myButton");
 
+myButton.classList.add("enabled");
+myButton.classList.remove("enabled");
+
+myButton.classList.add("hover");
+
+myButton.addEventListener("mouseover", event =>{
+    event.target.classList.add("hover");
+});
+myButton.addEventListener("mouseout", event =>{
+    event.target.classList.remove("hover");
+});
+
+myButton.addEventListener("mouseover", event =>{
+    event.target.classList.toggle("hover");
+});
+myButton.addEventListener("mouseout", event =>{
+    event.target.classList.toggle("hover");
+});
+
+myButton.classList.add("enabled");
+
+myButton.addEventListener("click", event => {
+    event.target.classList.replace("enabled", "disabled")}); 
+
+myButton.classList.add("enabled");
+
+myButton.addEventListener("click", event => {
+    if(event.target.classList.contains("disabled")){
+        event.target.textContent = "Enabled";
+    }
+    else{
+        event.target.textContent = "Disabled";
+    }
+    event.target.classList.replace("enabled", "disabled")});
