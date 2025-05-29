6b27b793b0c3223a5d390fdba81dca7d6a3a1469
SehwarMaryam <shehwarm93@gmail.com>
2025-05-29 15:46:32 +0500

    Remove button implementation and associated styles; introduce student object with printMarks method

--- first.js
+++ first.js
@@ -1,36 +1,10 @@
-const myButton = document.getElementById("myButton");
+const student = {
+    fullName : "John Doe",
+    age : 20,
+    marks : 94,
+    printMarks: function(){
+        console.log("Marks: ",marks);
+    },
 
-myButton.classList.add("enabled");
-myButton.classList.remove("enabled");
 
-myButton.classList.add("hover");
-
-myButton.addEventListener("mouseover", event =>{
-    event.target.classList.add("hover");
-});
-myButton.addEventListener("mouseout", event =>{
-    event.target.classList.remove("hover");
-});
-
-myButton.addEventListener("mouseover", event =>{
-    event.target.classList.toggle("hover");
-});
-myButton.addEventListener("mouseout", event =>{
-    event.target.classList.toggle("hover");
-});
-
-myButton.classList.add("enabled");
-
-myButton.addEventListener("click", event => {
-    event.target.classList.replace("enabled", "disabled")}); 
-
-myButton.classList.add("enabled");
-
-myButton.addEventListener("click", event => {
-    if(event.target.classList.contains("disabled")){
-        event.target.textContent = "Enabled";
-    }
-    else{
-        event.target.textContent = "Disabled";
-    }
-    event.target.classList.replace("enabled", "disabled")});
+};
