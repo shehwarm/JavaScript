9747245e5059d48380549b7a0e34d7bbe510e13a
SehwarMaryam <shehwarm93@gmail.com>
2025-05-29 16:28:09 +0500

    Refactor person objects: consolidate into a single person object with greet method

--- first.js
+++ first.js
@@ -1,31 +1,8 @@
-const person1 ={
- 
-    firstName: "Spongebob",
-    lastName : "docker",
-    age: 30,
-    isEmployed: true,
-    sayHello: function(){console.log("hiii")},
-    eat: function(){console.log("Im eating patty")},
-}
+const person = {
+  name: "Alice",
+  greet: function () {
+    console.log(`Hello, my name is ${this.name}`);
+  }
+};
 
-const person2 = {
-    
-    firstName: "patrick",
-    lastName : "docker",
-    age: 20,
-    isEmployed: false,
-    sayGoodbye: function(){console.log("byeee")},
-}
-
-console.log(person1.firstName);
-console.log(person1.lastName);
-console.log(person1.age);
-console.log(person1.isEmployed);
-person1.sayHello();
-person1.eat();
-
-console.log(person2.firstName);
-console.log(person2.lastName);
-console.log(person2.age);
-console.log(person2.isEmployed);
-person2.sayGoodbye();
+person.greet();  // Output: Hello, my name is Alice
