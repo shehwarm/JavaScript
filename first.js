6d9ae49194d463866ab5d9ddb0181716a1e3d032
SehwarMaryam <shehwarm93@gmail.com>
2025-06-03 18:54:03 +0500

    Remove unused JSON files for names, people, and person data

--- first.js
+++ first.js
@@ -1,9 +1,3 @@
- const jsonNames= `["squareBob" , "patrick", "squidward", "sandy"]`;
- const jsonPerson = `{"name": "spongebob", "age": 20, "isEmployed": true, "hobbies": ["jellyfishing","bubble blowing","karate"]}`;
- const jsonPeople = `[{"name": "spongebob", "age": 20,"isEmployed": true},
-                      {"name": "patrick", "age": 21, "isEmployed": false},
-                      {"name": "squidward", "age": 30,"isEmployed": true},
-                      {"name": "sandy","age": 25,"isEmployed": true}]`;
-const parsedData = JSON.parse(jsonPeople);
-
-console.log(parsedData);
+ fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+ .then(response => console.log(response))
+ .catch(error => console.error(error));
