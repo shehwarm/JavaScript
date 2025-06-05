ed783943838ca6f725bf01cb488823111a28eac8
SehwarMaryam <shehwarm93@gmail.com>
2025-06-05 22:30:14 +0500

    Refactor label elements and random number generation logic in JavaScript and HTML

--- first.js
+++ first.js
@@ -1,12 +1,20 @@
 const btn = document.getElementById('btn');
-const myLabel = document.getElementById('myLabel');
+const label1 = document.getElementById('label1');
+const label2 = document.getElementById('label2');
+const label3 = document.getElementById('label3');
 
 const min = 1;
 const max = 6;
 
-let randomNumber;
+let randomNumber1;
+let randomNumber2;
+let randomNumber3;
 
 btn.onclick = function(){
-    randomNumber = Math.floor(Math.random() * max) + min;
-    myLabel.textContent = randomNumber;
+    randomNumber1 = Math.floor(Math.random() * max) + min;
+    randomNumber2 = Math.floor(Math.random() * max) + min;
+    randomNumber3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNumber1;
+    label2.textContent = randomNumber2;
+    label3.textContent = randomNumber3;
 }
